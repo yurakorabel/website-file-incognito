b7f06719c3b0abd49bacf1c9a85353faeb821321
Yura <yurakorabel@gmail.com>
2024-12-08 02:56:20 +0200

    add download

--- public/js/upload.js
+++ public/js/upload.js
@@ -34,7 +34,7 @@ document.getElementById("uploadForm").addEventListener("submit", async function 
             };
 
             // Call the upload API
-            const response = await fetch("https://xvrmllf5w0.execute-api.us-east-1.amazonaws.com/run/upload", {
+            const response = await fetch("https://5bezw5qz7d.execute-api.us-east-1.amazonaws.com/run/upload", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
